819409ab57258d7ab9ffc259297e67d883ea0197
pvishnu04 <69616980+pvishnu04@users.noreply.github.com>
2024-11-10 08:15:12 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function fetchProducts() {
+    fetch('/api/products')
+      .then(response => response.json())
+      .then(products => {
+        let html = '<h2>Products</h2><ul>';
+        products.forEach(product => {
+          html += `<li>${product.name} - $${product.price}</li>`;
+        });
+        html += '</ul>';
+        document.getElementById('productList').innerHTML = html;
+      })
+      .catch(error => console.error('Error fetching products:', error));
+  }
+  
+  fetchProducts();
