76f59942a9558eca6d4c122f827630f5bea057f2
pvishnu04 <69616980+pvishnu04@users.noreply.github.com>
2024-11-23 21:38:36 +0000

    Update database.js

--- database.js
+++ database.js
@@ -1,43 +1,7 @@
-// database.js
 const sqlite3 = require('sqlite3').verbose();
 
-const db = new sqlite3.Database(':memory:', (err) => {
+const db = new sqlite3.Database('/db/pharmacy.db', (err) => {
   if (err) console.error('Failed to connect to the database', err);
   else console.log('Connected to the SQLite database.');
 });
-
-db.serialize(() => {
-  db.run(`
-    CREATE TABLE users (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      username TEXT NOT NULL,
-      email TEXT NOT NULL UNIQUE,
-      password TEXT NOT NULL,
-      role TEXT DEFAULT 'customer'
-    )
-  `);
-  
-  db.run(`
-    CREATE TABLE products (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      name TEXT NOT NULL,
-      description TEXT,
-      price REAL,
-      availability BOOLEAN DEFAULT 1,
-      category TEXT
-    )
-  `);
-  
-  db.run(`
-    CREATE TABLE orders (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      user_id INTEGER,
-      products TEXT,
-      total_price REAL,
-      order_date TEXT DEFAULT CURRENT_TIMESTAMP,
-      FOREIGN KEY(user_id) REFERENCES users(id)
-    )
-  `);
-});
-
 module.exports = db;
