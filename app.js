4566009a4005ebc4d13af8360fec5c1c9fc33585
pvishnu04 <69616980+pvishnu04@users.noreply.github.com>
2024-11-23 19:49:14 +0000

    Update app.js

--- app.js
+++ app.js
@@ -9,3 +9,19 @@ const orderRoutes = require('./routes/orderRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.send('Welcome to the Online Pharmacy API!');
+});
+app.use('/api/users', userRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/orders', orderRoutes);
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
