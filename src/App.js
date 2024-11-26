6c98eb8815c65f226b83aa5ef2c8bb99469f29ad
pvishnu04 <69616980+pvishnu04@users.noreply.github.com>
2024-11-26 16:24:22 +0000

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Register from './components/Register';
+import Login from './components/Login';
