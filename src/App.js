58b22da5e8c1b96da9ea582e19b8035f9be0b424
pvishnu04 <69616980+pvishnu04@users.noreply.github.com>
2024-11-26 16:35:51 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,3 +3,19 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Register from './components/Register';
 import Login from './components/Login';
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
