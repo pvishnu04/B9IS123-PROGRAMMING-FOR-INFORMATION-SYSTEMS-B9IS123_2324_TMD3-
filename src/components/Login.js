c5cf552be23dace226afb692ef719e23be12f2ed
pvishnu04 <69616980+pvishnu04@users.noreply.github.com>
2024-11-26 18:40:46 +0000

    Create Login.js

---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Login = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
