0a729e3c9acafd28eec10194a724cf0fb2bbed83
pvishnu04 <69616980+pvishnu04@users.noreply.github.com>
2024-11-29 18:01:20 +0000

    Update Register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -24,3 +24,26 @@ const Register = () => {
             alert('Registration failed. Please try again.');
         }
     };
+    return (
+        <div>
+            <h2>Register</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Name:</label>
+                    <input type="text" name="name" value={formData.name} onChange={handleChange} required />
+                </div>
+                <div>
+                    <label>Email:</label>
+                    <input type="email" name="email" value={formData.email} onChange={handleChange} required />
+                </div>
+                <div>
+                    <label>Password:</label>
+                    <input type="password" name="password" value={formData.password} onChange={handleChange} required />
+                </div>
+                <button type="submit">Register</button>
+            </form>
+        </div>
+    );
+};
+
+export default Register;
