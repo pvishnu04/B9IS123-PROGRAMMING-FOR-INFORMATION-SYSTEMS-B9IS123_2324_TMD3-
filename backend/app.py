b07072166abbc3750535e4aab45f9c280f8a08ed
pvishnu04 <69616980+pvishnu04@users.noreply.github.com>
2024-12-04 12:06:06 +0000

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -7,3 +7,12 @@ from flask_cors import CORS
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # For session management (change this)
 CORS(app)
+DATABASE_CONFIG = {
+    "host": "localhost",
+    "port": 5432,
+    "user": "postgres",  
+    "password": "admin",  
+    "database": "Pharmacy"  
+}
+def get_db_connection():
+    return pg8000.connect(**DATABASE_CONFIG)
