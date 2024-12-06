662e3f797e1f0c07b3e1956f6e764e065562c5ac
pvishnu04 <69616980+pvishnu04@users.noreply.github.com>
2024-12-06 16:17:38 +0000

    Deleted database.js

--- database.js
+++
@@ -1,7 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-
-const db = new sqlite3.Database('/db/pharmacy.db', (err) => {
-  if (err) console.error('Failed to connect to the database', err);
-  else console.log('Connected to the SQLite database.');
-});
-module.exports = db;
