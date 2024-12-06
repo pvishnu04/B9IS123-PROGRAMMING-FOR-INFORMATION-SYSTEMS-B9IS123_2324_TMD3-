5ff3be9a5fee0c84e831c3c1f5500e7662005ffd
pvishnu04 <69616980+pvishnu04@users.noreply.github.com>
2024-12-06 16:14:10 +0000

    Modified Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1>Welcome to the Medicine Store</h1>
+      <nav>
+      <Link to="/register">Register</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/contactUs">Contact Us</Link>
+      </nav>
+  </div>
+  );
+};
+export default Home;
